23963a90f1a7be68cae3dfbfe30ba0279f539c8e
Nhoni <nhmbemba@gmail.com>
2023-12-05 12:23:43 +0100

    🚧

---
+++ forum/partials/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form in PHP</title>
+    <link rel="icon" href="https://api.iconify.design/logos:php-alt.svg" type="image/svg+xml">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;400;600&display=swap" rel="stylesheet">
+</head>
+
+<body style="font-family: 'Inter', sans-serif;">
+    <div class="container" style="padding:1em; text-align:center;">
+        <kbd>PHP FORUM</kbd>
+    </div>
+    <main class="container">
